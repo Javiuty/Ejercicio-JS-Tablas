3681815c44c37eabe7f77b6c4c8efd791edcf3ba
Javiuty <javiiescuadra@gmail.com>
2020-12-11 13:09:26 +0100

    exercise 1 finished

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //  Variables
 const tabla = document.querySelector(".tabla");
 const tablaHeading = document.querySelector(".tabla__heading");
+const tablaRow = document.querySelector(".tabla__row");
 
 // Funciones
 function callingData() {
@@ -17,9 +18,15 @@ function renderingHTML(data) {
     td.textContent = prop;
     tablaHeading.appendChild(td);
   }
+
+  for (let i = 0; i < data.length; i++) {
+    const tr = document.createElement("tr");
+
+    for (let prop in data[i]) {
+      const td = document.createElement("td");
+      td.textContent = data[i][prop];
+      tr.appendChild(td);
+      tablaHeading.appendChild(tr);
+    }
+  }
 }
-
-// tabla.innerHTML = `
-//     <th>id</th>
-
-// `;
